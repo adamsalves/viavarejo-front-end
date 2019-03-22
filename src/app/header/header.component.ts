30f443515cf7a456e49e501c97ddb696bf70e458
Adams Alves <alves.adams@gmail.com>
2019-03-22 17:35:30 -0300

    adjust resposive layout: show menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   constructor() { }
-
+  private active : boolean = false;
   ngOnInit() {
+  }
+
+  onClickMenu() {
+    this.active = !this.active
   }
 
 }
